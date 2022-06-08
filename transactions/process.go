fccf994e2f925422629348472d88c168ab5008a4
Shane Huston <hustonr@tcd.ie>
2022-06-08 12:36:31 +0100

    Handle unspecified input files

--- transactions/process.go
+++ transactions/process.go
@@ -10,11 +10,15 @@ func Process(revolutFile string, aibFile string, startDate time.Time) {
   fmt.Println("Excluding transactions on and before ", startDate)
   report := NewReport(startDate)
 
-  revolutLines := utils.ReadInputFile(revolutFile)
-  aibLines := utils.ReadInputFile(aibFile)
+  if revolutFile != "" {
+    revolutLines := utils.ReadInputFile(revolutFile)
+    report = report.AddTransactions(revolutLines, true)
+  }
 
-  report = report.AddTransactions(revolutLines, true)
-  report = report.AddTransactions(aibLines, false)
+  if aibFile != "" {
+    aibLines := utils.ReadInputFile(aibFile)
+    report = report.AddTransactions(aibLines, false)
+  }
 
   report.Sort()
   report.Output()
