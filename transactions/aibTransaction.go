5f3d02ce42c901c36dba196449433f24754e5e58
Shane Huston <stonish@github.com>
2024-06-23 17:43:37 +0100

    Update to correct username in paths, plus whitespace changes

--- transactions/aibTransaction.go
+++ transactions/aibTransaction.go
@@ -1,138 +1,139 @@
 package transactions
 
 import (
-  "fmt"
-  "log"
-  "strings"
-  "time"
-  "strconv"
-  "math"
-  "errors"
-  "github.com/shuston/banking/utils"
+	"errors"
+	"fmt"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/stonish/banking/utils"
 )
 
 type aibTransaction struct {
-  id string
-  realDate string
-  completedDate time.Time
-  details string
-  amount float64
-  balance string
-  source string
-  isHidden bool
+	id            string
+	realDate      string
+	completedDate time.Time
+	details       string
+	amount        float64
+	balance       string
+	source        string
+	isHidden      bool
 }
 
 func NewAIBTransaction(rawData string) *aibTransaction {
-  t := new(aibTransaction)
+	t := new(aibTransaction)
 
-  data := strings.Split(rawData, ",")
+	data := strings.Split(rawData, ",")
 
-  transactionDate, err := time.Parse("02/01/06", data[1])
-  if err != nil {
-    log.Fatal("Could not parse date: ", err, "\nRaw data: ", rawData)
-  }
+	transactionDate, err := time.Parse("02/01/06", data[1])
+	if err != nil {
+		log.Fatal("Could not parse date: ", err, "\nRaw data: ", rawData)
+	}
 
-  t.completedDate = transactionDate
+	t.completedDate = transactionDate
 
-  t.details = strings.Trim(data[2], " ")
-  t.details = strings.Replace(t.details, "\"", "", -1)
+	t.details = strings.Trim(data[2], " ")
+	t.details = strings.Replace(t.details, "\"", "", -1)
 
-  t.amount, err = t.parseAmount(data[3], data[4])
-  if err != nil {
-    log.Fatal("Could not parse amount: ", err, "\nRaw data: ", rawData)
-  }
+	t.amount, err = t.parseAmount(data[3], data[4])
+	if err != nil {
+		log.Fatal("Could not parse amount: ", err, "\nRaw data: ", rawData)
+	}
 
-  t.balance = data[5]
-  t.source = "AIB"
-        t.isHidden = t.shouldHide()
-  t.id = utils.Classify(t.details)
+	t.balance = data[5]
+	t.source = "AIB"
+	t.isHidden = t.shouldHide()
+	t.id = utils.Classify(t.details)
 
-  return t
+	return t
 }
 
 func (t aibTransaction) GetCompletedDate() time.Time {
-  return t.completedDate
+	return t.completedDate
 }
 
 func (t aibTransaction) Output() {
-  if (t.isHidden) {
-    return
-  }
+	if t.isHidden {
+		return
+	}
 
-  roundedAmount := math.Round(t.amount*100)/100
-  strAmount := fmt.Sprintf("%f", roundedAmount)
-  fmt.Println(t.id + "\t" +
-    t.realDate + "\t" +
-    t.completedDate.Format("2 Jan") + "\t" +
-    strAmount + "\t" +
-    t.details + "\t" +
-    t.source)
+	roundedAmount := math.Round(t.amount*100) / 100
+	strAmount := fmt.Sprintf("%f", roundedAmount)
+	fmt.Println(t.id + "\t" +
+		t.realDate + "\t" +
+		t.completedDate.Format("2 Jan") + "\t" +
+		strAmount + "\t" +
+		t.details + "\t" +
+		t.source)
 }
 
 func (t aibTransaction) parseAmount(debitAmount string, creditAmount string) (float64, error) {
-  var amount float64
+	var amount float64
 
-  if debitAmount != "" && creditAmount != "" {
-    return amount, errors.New("Transaction with both credit and debit amounts found")
-  }
+	if debitAmount != "" && creditAmount != "" {
+		return amount, errors.New("Transaction with both credit and debit amounts found")
+	}
 
-  if debitAmount != "" {
-    amount, err := strconv.ParseFloat(debitAmount, 32)
+	if debitAmount != "" {
+		amount, err := strconv.ParseFloat(debitAmount, 32)
 
-    if err != nil {
-      return amount, fmt.Errorf("Could not parse amount: %w", err)
-    }
+		if err != nil {
+			return amount, fmt.Errorf("Could not parse amount: %w", err)
+		}
 
-    return amount, nil
-  }
+		return amount, nil
+	}
 
-  if creditAmount != "" {
-    amount, err := strconv.ParseFloat(creditAmount, 32)
+	if creditAmount != "" {
+		amount, err := strconv.ParseFloat(creditAmount, 32)
 
-    if err != nil {
-      return amount, fmt.Errorf("Could not parse amount: %w", err)
-    }
+		if err != nil {
+			return amount, fmt.Errorf("Could not parse amount: %w", err)
+		}
 
-    if amount != 0 {
-      amount = -1 * amount
-    }
+		if amount != 0 {
+			amount = -1 * amount
+		}
 
-    return amount, nil
-  }
+		return amount, nil
+	}
 
-  return amount, errors.New("Transaction with neither credit nor debit amounts found")
+	return amount, errors.New("Transaction with neither credit nor debit amounts found")
 }
 
 func (t aibTransaction) shouldHide() bool {
-  if (t.amount == 0) {
-    return true
-  }
+	if t.amount == 0 {
+		return true
+	}
 
-  if t.isRevolutTopUp() {
-    return true
-  }
+	if t.isRevolutTopUp() {
+		return true
+	}
 
-  if strings.HasPrefix(t.details, "*INET SAVINGS ") {
-    return true
-  }
+	if strings.HasPrefix(t.details, "*INET SAVINGS ") {
+		return true
+	}
 
-  if t.amount == 600 &&
-    (strings.HasPrefix(t.details, "*INET RENT ") ||
-     strings.HasPrefix(t.details, "*INET DAVID ")) {
-    return true
-  }
+	if t.amount == 600 &&
+		(strings.HasPrefix(t.details, "*INET RENT ") ||
+			strings.HasPrefix(t.details, "*INET DAVID ")) {
+		return true
+	}
 
-  if t.amount == 10 && t.details == "931365 22689017" {
-    return true
-  }
+	if t.amount == 10 && t.details == "931365 22689017" {
+		return true
+	}
 
-  return false
+	return false
 }
 
 func (t aibTransaction) isRevolutTopUp() bool {
-  return strings.HasPrefix(t.details, "VDP-Revolut**") ||
-         strings.HasPrefix(t.details, "VDP-Revolut* - ") ||
-         strings.HasPrefix(t.details, "VDP-Revolut  - ") ||
-         strings.HasPrefix(t.details, "VDP-REVOLUT*") ||
-         t.details == "VDP-Revolut"
+	return strings.HasPrefix(t.details, "VDP-Revolut**") ||
+		strings.HasPrefix(t.details, "VDP-Revolut* - ") ||
+		strings.HasPrefix(t.details, "VDP-Revolut  - ") ||
+		strings.HasPrefix(t.details, "VDP-REVOLUT*") ||
+		t.details == "VDP-Revolut"
 }
--- transactions/revolutTransaction.go
+++ transactions/revolutTransaction.go
@@ -1,105 +1,106 @@
 package transactions
 
 import (
-  "fmt"
-  "log"
-  "strings"
-  "time"
-  "strconv"
-  "math"
-  "github.com/shuston/banking/utils"
+	"fmt"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/stonish/banking/utils"
 )
 
 type revolutTransaction struct {
-  id string
-  transactionType string
-  product string
-  startedDate time.Time
-  completedDate time.Time
-  description string
-  amount float64
-  fee float64
-  currency string
-  state string
-  source string
-  balance float64
-  isHidden bool
+	id              string
+	transactionType string
+	product         string
+	startedDate     time.Time
+	completedDate   time.Time
+	description     string
+	amount          float64
+	fee             float64
+	currency        string
+	state           string
+	source          string
+	balance         float64
+	isHidden        bool
 }
 
 func NewRevolutTransaction(rawData string) *revolutTransaction {
-  data := strings.Split(rawData, ",")
+	data := strings.Split(rawData, ",")
 
-  t := new(revolutTransaction)
-  t.transactionType = data[0]
-  t.product = data[1]
+	t := new(revolutTransaction)
+	t.transactionType = data[0]
+	t.product = data[1]
 
-  startedDate, err := time.Parse("2006-01-02 15:04:05", data[2])
-  if err != nil {
-    log.Fatal("Could not parse date: ", err, "\nRaw data: ", rawData)
-  }
-  t.startedDate = startedDate
+	startedDate, err := time.Parse("2006-01-02 15:04:05", data[2])
+	if err != nil {
+		log.Fatal("Could not parse date: ", err, "\nRaw data: ", rawData)
+	}
+	t.startedDate = startedDate
 
-  completedDateStr := data[3]
-  if completedDateStr != "" {
-    completedDate, err := time.Parse("2006-01-02 15:04:05", completedDateStr)
-    if err != nil {
-      log.Fatal("Could not parse date: ", err, "\nRaw data: ", rawData)
-    }
-    t.completedDate = completedDate
-  }
+	completedDateStr := data[3]
+	if completedDateStr != "" {
+		completedDate, err := time.Parse("2006-01-02 15:04:05", completedDateStr)
+		if err != nil {
+			log.Fatal("Could not parse date: ", err, "\nRaw data: ", rawData)
+		}
+		t.completedDate = completedDate
+	}
 
-  t.description = strings.Trim(data[4], " ")
+	t.description = strings.Trim(data[4], " ")
 
-  amount, err := strconv.ParseFloat(data[5], 64)
-  if err != nil {
-    log.Fatal("Could not parse amount: ", err, "\nRaw data: ", rawData)
-  }
+	amount, err := strconv.ParseFloat(data[5], 64)
+	if err != nil {
+		log.Fatal("Could not parse amount: ", err, "\nRaw data: ", rawData)
+	}
 
-  fee, err := strconv.ParseFloat(data[6], 64)
-  if err != nil {
-    log.Fatal("Could not parse fee: ", err, "\nRaw data: ", rawData)
-  }
-  t.fee = fee
-  t.amount = -amount + t.fee
+	fee, err := strconv.ParseFloat(data[6], 64)
+	if err != nil {
+		log.Fatal("Could not parse fee: ", err, "\nRaw data: ", rawData)
+	}
+	t.fee = fee
+	t.amount = -amount + t.fee
 
-  t.currency = data[7]
-  t.state = data[8]
+	t.currency = data[7]
+	t.state = data[8]
 
-  balanceStr := data[9]
-  if balanceStr != "" {
-    balance, err := strconv.ParseFloat(data[9], 64)
-    if err != nil {
-      log.Fatal("Could not parse balance: ", err, "\nRaw data: ", rawData)
-    }
-    t.balance = balance
-  }
+	balanceStr := data[9]
+	if balanceStr != "" {
+		balance, err := strconv.ParseFloat(data[9], 64)
+		if err != nil {
+			log.Fatal("Could not parse balance: ", err, "\nRaw data: ", rawData)
+		}
+		t.balance = balance
+	}
 
-  t.source = "Revolut"
-  t.isHidden = t.shouldHide()
-  t.id = utils.Classify(t.description)
+	t.source = "Revolut"
+	t.isHidden = t.shouldHide()
+	t.id = utils.Classify(t.description)
 
-  return t
+	return t
 }
 
 func (t revolutTransaction) GetCompletedDate() time.Time {
-  return time.Date(t.completedDate.Year(), t.completedDate.Month(), t.completedDate.Day(), 0, 0, 0, 0, time.UTC)
+	return time.Date(t.completedDate.Year(), t.completedDate.Month(), t.completedDate.Day(), 0, 0, 0, 0, time.UTC)
 }
 
 func (t revolutTransaction) Output() {
-  if t.isHidden {
-    return
-  }
+	if t.isHidden {
+		return
+	}
 
-  roundedAmount := math.Round(t.amount*100)/100
-  strAmount := fmt.Sprintf("%f", roundedAmount)
-  fmt.Println(t.id + "\t" +
-    t.startedDate.Format("2 Jan") + "\t" +
-    t.completedDate.Format("2 Jan") + "\t" +
-    strAmount + "\t" +
-    t.description + "\t" +
-    t.source)
+	roundedAmount := math.Round(t.amount*100) / 100
+	strAmount := fmt.Sprintf("%f", roundedAmount)
+	fmt.Println(t.id + "\t" +
+		t.startedDate.Format("2 Jan") + "\t" +
+		t.completedDate.Format("2 Jan") + "\t" +
+		strAmount + "\t" +
+		t.description + "\t" +
+		t.source)
 }
 
 func (t revolutTransaction) shouldHide() bool {
-  return t.amount == 0 || t.transactionType == "TOPUP" || t.state == "PENDING"
+	return t.amount == 0 || t.transactionType == "TOPUP" || t.state == "PENDING"
 }
